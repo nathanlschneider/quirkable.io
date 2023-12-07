851f9bd85c08450551cdcc5c5818d0e9dda1915f
Nathan Schneider <nathan@quirkable.io>
2023-12-06 22:57:42 -0500

    Added dictionary quote

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -8,14 +8,33 @@ const About = (props) => {
 	return (
 		<section className={styles.about}>
 			{/* <Curve/> */}
-			<section className={styles.about_inner}>
-			<h2 className={styles.h2}>Stuff You Need To Know.</h2>
-			<div className={styles.body_a}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam sunt laborum molestiae soluta esse provident, modi, dolorem dolore tenetur minus, eos officiis voluptatibus animi voluptas facilis ipsam in officia ducimus!</div>
-			<div className={styles.body_b}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam sunt laborum molestiae soluta esse provident, modi, dolorem dolore tenetur minus, eos officiis voluptatibus animi voluptas facilis ipsam in officia ducimus!</div>
-			<div className={styles.body_c}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam sunt laborum molestiae soluta esse provident, modi, dolorem dolore tenetur minus, eos officiis voluptatibus animi voluptas facilis ipsam in officia ducimus!</div>
-			<div className={styles.body_d}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam sunt laborum molestiae soluta esse provident, modi, dolorem dolore tenetur minus, eos officiis voluptatibus animi voluptas facilis ipsam in officia ducimus!</div>
-			<Image className={styles.cube} src='/images/wheel.webp' width={600} height={600} alt='' />
 
+			<section className={styles.about_inner}>
+				<h2 className={styles.h2}>Stuff You Need To Know.</h2>
+				<article className={styles.quote}>
+					<div className={styles.quote_title}>quir·ka·ble</div>
+					<div className={styles.quote_pronun} id='pronunciation'>
+						<p>/kwɜːrkəbl/</p>
+					</div>
+					<ol className={styles.quote_list}>
+						<li>
+							<strong>Adjective</strong>
+							<p>Capable of embracing and adapting to unique or unconventional qualities.</p>
+							<em>The quirkable software effortlessly accommodated the diverse needs of its users.</em>
+						</li>
+						<li>
+							<p>
+								Pertaining to a quality or feature that is distinctive, peculiar, or characterized by
+								individuality.
+							</p>
+							<em>
+								The quirkable design of the product set it apart in a market saturated with standard
+								offerings.
+							</em>
+						</li>
+					</ol>
+				</article>
+				<Image className={styles.cube} src='/images/wheel.webp' width={600} height={600} alt='' />
 			</section>
 		</section>
 	);
